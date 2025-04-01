baab45978a3e42e35094def88f12dcbd79170f85
Magesh Dhasayyan <mgsh@users.noreply.github.com>
2025-04-01 11:59:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import ExifTest3Image from "./assets/exif-test-3.jpg";
 import { ImageOrientationTest } from "./ImageOrientationTest";
